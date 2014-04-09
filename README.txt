9a50a4b317a9db7b8ceedafd90bf6bc7509a590e
ATB office <ping.x86@gmail.com>
2014-04-10 06:50:39 +1000

    Fill-script.

--- README.txt
+++ README.txt
@@ -1,8 +1,15 @@
-﻿Список модулей:
-flask
-flask-sqlalchemy
-flask-wtf
-flask-login
-flask-migrate
-pillow 2.3.0 (binary for Windows)
-Flask-WhooshAlchemy
+﻿Description:
+Форум (регистрация, профили, поиск по сообщениям, личные сообщения, статистика).
+
+Components:
+Python (2.7.6)
+flask (0.10.1)
+flask-sqlalchemy (1.0)
+flask-wtf (0.9.5)
+flask-login (0.2.10)
+flask-migrate (1.2.0)
+pillow (2.3.0, binary for Windows)
+Flask-WhooshAlchemy (0.55)
+
+Twitter Bootstrap (3.1.1)
+jQuery (1.11.0)
--- app/views.py
+++ app/views.py
@@ -9,6 +9,7 @@ func = sqlalchemy.func
 from datetime import datetime
 import hashlib
 import os
+import random
 from cgi import escape
 from math import ceil
 from PIL import Image, ImageOps
@@ -752,6 +753,112 @@ def mail_move(directory, message_id):
     # Вернуться на страницу, откуда было вызвано удаление
     return(redirect(request.referrer))
 
+
+# --- ЗАПОЛНИТЬ ФОРУМ СООБЩЕНИЯМИ ---------------
+@app.route('/fill', methods=['GET', 'POST'])
+@login_required
+def fill():
+    if request.method == 'POST':
+        FILE = 'text.txt'
+        file_size = os.stat(FILE).st_size
+
+        # Создать тему
+        def make_topic(file_str, file_size):
+            '''
+            Функция открывает файл со случайным смещением и
+            и возвращает от 1 до 3 слов, идущих подряд
+            '''
+            
+            random.seed()
+            offset = random.randrange(0, file_size-1)
+            file = open(file_str)
+            file.seek(offset)
+
+            topic = ''
+            started = False
+            end = [' ', '.', '!', '?', '\n']
+            number = random.randrange(1, 4)
+            for line in file:
+                for char in line:
+                    # Если слово ещё не началось
+                    if not started:
+                        if char not in end:
+                            continue
+                        else:
+                            started = True
+                    # Если предложение уже в записи
+                    else:
+                        # Ждать один из символов окончания предложения
+                        if char in end:
+                            topic += char
+                            number -= 1
+                            break
+                        # Если не конец, то продолжать записывать предложение
+                        else:
+                            topic += char            
+                if not number and started and topic[-1] in end:
+                    topic = topic.replace('\n', ' ')[:-1]
+                    return(topic)
+            
+        # Создать сообщение
+        def make_message(file_str, file_size):
+            '''
+            Функция открывает файл со случайным смещением и
+            и возвращает от 1 до 7 предложений, идущих подряд
+            '''
+            
+            random.seed()
+            offset = random.randrange(0, file_size-1)
+            file = open(file_str)
+            file.seek(offset)
+            
+            message = ''
+            started = False
+            end = ['.', '!', '?']
+            number = random.randrange(1, 8)
+            for line in file:
+                for char in line:
+                    # Если предложение ещё не началось
+                    if not started:
+                        if char not in end:
+                            continue
+                        else:
+                            started = True
+                    # Если предложение уже в записи
+                    else:
+                        # Ждать один из символов окончания предложения
+                        if char in end:
+                            message += char
+                            number -= 1
+                            break
+                        # Если не конец, то продолжать записывать предложение
+                        else:
+                            message += char            
+                if not number and started and message[-1] in end:
+                    message = message.replace('\n', ' ')
+                    return(message)
+    
+        new_topic = ForumTopic(name=make_topic(FILE, file_size), author_id=current_user.id)
+        current_user.message_count += 1
+        current_user.topic_count += 1
+        db.session.add(new_topic)
+        # Коммит в этом месте нужен, чтобы появился ID
+        db.session.commit()
+        # Создание сообщения
+        new_mes = ForumMessage(topic_id=new_topic.id, author_id=current_user.id, text=make_message(FILE, file_size))
+        db.session.add(new_mes)
+        db.session.commit()
+        
+        for count in range(1000):
+            new_mes = ForumMessage(topic_id=new_topic.id, author_id=random.randrange(1, 4), text=make_message(FILE, file_size))
+            db.session.add(new_mes)
+        db.session.commit()
+        
+  
+        return(redirect(request.referrer))
+
+    return(render_template('fill.html',
+        user=current_user))
 
 # --- ВЫХОД -------------------------------------
 @app.route('/logout')
